785404357f2c885dc11996320dcb8dc815d9c54b
Koding <ttdennis@ttdennis.koding.io>
2016-02-20 04:12:15 -0600

    JSHint works now

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,9 @@ const notify = require('gulp-notify');
 const growl = require('gulp-notify-growl');
 const jscs = require('gulp-jscs');
 const jshint = require('gulp-jshint');
+const changed = require('gulp-changed')
 
-
-gulp.task('jscs', function() {
-    gulp.src('./lib/*.js')
-        .pipe(jscs())
-        .pipe(notify({
-            title: 'JSCS',
-            message: 'JSCS Passed. Let it fly!'
-        }));
+gulp.task('jshint', function () {
+    const lib = gulp.src('lib/**/*.js').pipe(changed('lib/*.js')).pipe(jshint()).pipe(jshint.reporter('jshint-stylish'));
+    const routes = gulp.src('routes/*.js').pipe(changed('routes/*.js')).pipe(jshint()).pipe(jshint.reporter('jshint-stylish'));
 });
