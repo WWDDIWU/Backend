fd29af41f9bf976c31a8daff276539654ac6ada7
tobuto <tom.burmeister@ymail.com>
2016-02-22 00:26:44 -0600

    Sort Day Could should with DB now, removed Google API Key

--- test/day-test.js
+++ test/day-test.js
@@ -6,7 +6,7 @@ const path = require('path');
 const mocha = require('mocha');
 const should = require('should');
 
-const dayLib = require("../lib/day");
+const dayLib = require("../lib/day.custom");
 
 describe('Test Day Sorting', function () {
     it ('should bla', function(done) {
@@ -14,12 +14,12 @@ describe('Test Day Sorting', function () {
             owner: 1,
             date: Date('2016-02-20'),
             checksum: 'aaaaaaaaaaaaaaaaaaaaaaaa',
-            events: [0,1,2,3,4,5],
+            events: [0,1,2,3,4],
             timeToGetFromAtoB: [],
             sequence: []
         };
         
-        dayLib.sortDay(testDay, function (seq) {
+        dayLib.sortDay(testDay, function (err, seq, ttgfatb) {
             console.log(seq);
             done();
         });
